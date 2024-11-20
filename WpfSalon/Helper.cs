bab59d6e9f94a12823a161e001626f4c730da821
pr-21.102-gn <pr-21.102-gn@352-12>
2024-11-20 16:49:13 +0700

    Добавьте файлы проекта.

---
+++ WpfSalon/Helper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfSalon
+{
+    internal class Helper
+    {
+        public static Entities ent;
+
+        public static Entities GetContext()
+        {
+            if (ent == null) {
+                ent = new Entities();
+            }
+            return ent;
+        }
+    } 
+}
+
